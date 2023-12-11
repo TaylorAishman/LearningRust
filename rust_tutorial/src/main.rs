b4ed3abd9e9d790e6003861eaa5cae2b30a9112a
TaylorAishman <tayloraishman@outlook.com>
2023-12-11 04:46:43 -0600

    Booleans and Characters

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -7,11 +7,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Max u32 : {}", u32::MAX);
-    println!("Max u64 : {}", u64::MAX);
-    println!("Max usize : {}", usize::MAX);
-    println!("Max u128 : {}", u128::MAX);
-    println!("Max f32 : {}", f32::MAX);
-    println!("Max f64 : {}", f64::MAX);
-    
+    let is_true = true;
+    let my_grade = 'A';
+
 }
